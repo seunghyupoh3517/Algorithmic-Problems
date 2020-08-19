d2281852e7d48a9e527dfffe3db2f6048ef659b5
Seunghyup Alex Oh <seunghyupoh3517@gmail.com>
2020-08-18 20:56:08 -0700

    Add files via upload

---
+++ Makefile Tutorial/function2.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include "functions.h"
+void print_hello(){
+    std::cout << "Hello World!";
+}
---
+++ Makefile Tutorial/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include "functions.h"
+using namespace std;
+int main(){
+
+    print_hello();
+    std::cout<<std::endl;
+    std::cout << "The factorial of 5 is " << factorial(5) << std:: endl;
+
+    return 0;
+}
+
